ed1d447ef7bfadcf2ce9f661917b57714f63bbe3
ibeomjin <dlqjawls98@naver.com>
2021-09-20 02:05:00 +0900

    Second commit

--- DB_site/settings.py
+++ DB_site/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'DB_polls.apps.DbPollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
